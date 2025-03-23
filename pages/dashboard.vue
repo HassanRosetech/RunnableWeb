bab389f42b33a07be275a0bcaefd11e971b5ec2e
Hassan Barbour <barbour.hassan@gmail.com>
2025-03-24 00:07:08 +0300

    dashboard design

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,40 +1,38 @@
 <template>
   <v-row class="month-table">
     <v-col cols="12" sm="12">
-      <UiChildCard title="Contacts Table">
-        <v-table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Email</th>
-              <th>Department</th>
-              <th>Message</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(contacts, index) in contacts">
-              <th scope="row">{{ index + 1 }}</th>
-              <td>{{ contacts.first_name }}</td>
-              <td>{{ contacts.last_name }}</td>
-              <td>{{ contacts.email }}</td>
-              <td>{{ contacts.department }}</td>
-              <td>{{ contacts.message }}</td>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Department</th>
+            <th>Message</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(contacts, index) in contacts">
+            <th scope="row">{{ index + 1 }}</th>
+            <td>{{ contacts.first_name }}</td>
+            <td>{{ contacts.last_name }}</td>
+            <td>{{ contacts.email }}</td>
+            <td>{{ contacts.department }}</td>
+            <td>{{ contacts.message }}</td>
 
-              <td>
-                <button
-                  type="button"
-                  class="btn btn-danger"
-                  @click="($event) => deleteContact(contacts.id)"
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </v-table>
-      </UiChildCard>
+            <td>
+              <button
+                type="button"
+                class="btn btn-danger"
+                @click="($event) => deleteContact(contacts.id)"
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </v-col>
   </v-row>
 </template>
@@ -64,3 +62,9 @@ definePageMeta({
   layout: "dd",
 });
 </script>
+<style>
+th,
+td {
+  width: 250px;
+}
+</style>
