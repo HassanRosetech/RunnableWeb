73b8f94e5fd57208ac16f7d1e8e87df9daa1b376
Hassan Barbour <barbour.hassan@gmail.com>
2025-04-19 19:13:23 +0300

    content update22

--- components/Home/LatestProjects.vue
+++ components/Home/LatestProjects.vue
@@ -22,31 +22,63 @@
         <!-- /column -->
       </div>
 
-      <UCarousel v-slot="{ item }" arrows :items="items">
-        <img :src="item" width="540" height="540" class="rounded-lg" />
-      </UCarousel>
-      <!-- /.row -->
-
-      <!-- /.swiper-container -->
-    </div>
-    <!-- /.container -->
-    <!-- <div class="divider !text-[#f0f8fe] mx-[-0.5rem]">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 100">
-        <path
-          fill="currentColor"
-          d="M1260,1.65c-60-5.07-119.82,2.47-179.83,10.13s-120,11.48-180,9.57-120-7.66-180-6.42c-60,1.63-120,11.21-180,16a1129.52,1129.52,0,0,1-180,0c-60-4.78-120-14.36-180-19.14S60,7,30,7H0v93H1440V30.89C1380.07,23.2,1319.93,6.15,1260,1.65Z"
+      <UCarousel v-slot="{ item }" arrows :items="items" v-model="current">
+        <img
+          :src="item.image"
+          width="540"
+          height="540"
+          class="rounded-lg mx-auto"
         />
-      </svg>
-    </div> -->
+
+        <!-- Content Section -->
+        <div class="text-center mt-4 px-4">
+          <h3 class="text-2xl font-semibold text-gray-800 mb-2 md:text-3xl">
+            {{ item.title }}
+          </h3>
+
+          <p
+            class="text-gray-600 text-sm leading-relaxed mx-auto max-w-prose md:text-base"
+          >
+            {{ item.description }}
+          </p>
+        </div>
+      </UCarousel>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount } from "vue";
 const items = [
-  "https://picsum.photos/640/640?random=1",
-  "https://picsum.photos/640/640?random=2",
-  "https://picsum.photos/640/640?random=3",
-  "https://picsum.photos/640/640?random=4",
-  "https://picsum.photos/640/640?random=5",
-  "https://picsum.photos/640/640?random=6",
+  {
+    image: "/images/projects/DELIVERY.png?random=1",
+    title: "Tower Internal Project",
+    description:
+      "A secure enterprise-grade application for managing internal processes and technical operations.",
+  },
+  {
+    image: "/images/projects/DELIVERY.png?random=2",
+    title: "jOrder",
+    description:
+      "A scalable white-label delivery SaaS platform for providers to manage and dispatch orders with ease.",
+  },
+  {
+    image: "/images/projects/DELIVERY.png?random=3",
+    title: "Nidan Internal Projects",
+    description:
+      " A suite of tools for internal analytics, resource management, and workflow automation.",
+  },
 ];
+
+const current = ref(0);
+let interval: ReturnType<typeof setInterval>;
+
+onMounted(() => {
+  interval = setInterval(() => {
+    current.value = (current.value + 1) % items.length;
+  }, 1000); // 3 seconds interval
+});
+
+onBeforeUnmount(() => {
+  clearInterval(interval);
+});
 </script>
