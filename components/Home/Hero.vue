f0f8f1e3d20d35a82a78e0c7d0f6e275dd079a94
Hassan Barbour <barbour.hassan@gmail.com>
2025-05-12 12:27:41 +0300

    animation

--- components/Home/Hero.vue
+++ components/Home/Hero.vue
@@ -1,67 +1,51 @@
 <template>
   <section id="about" class="wrapper !bg-[#ffffff]">
-    <div
-      class="container pt-10 xl:pt-[4.5rem] lg:pt-[4.5rem] md:pt-[4.5rem] pb-[4.5rem] xl:pb-24 lg:pb-24 md:pb-24 !text-center"
-    >
-      <div
-        class="flex flex-wrap mx-[-15px] xl:mx-[-35px] lg:mx-[-20px] !mt-[-50px] xl:!mt-0 !mb-[4.5rem] items-center"
-      >
-        <div
-          class="xl:w-7/12 lg:w-7/12 w-full flex-[0_0_auto] !px-[15px] xl:!px-[35px] lg:!px-[20px] !mt-[50px] xl:!mt-0 max-w-full xl:!order-2 lg:!order-2"
-        >
-          <figure class="m-0 p-0">
-            <img
-              class="img-auto"
-              src="~/assets/img/illustrations/i21.png"
-              srcset="~/assets/img/illustrations/i21@2x.png 2x"
-              alt="image"
-            />
-          </figure>
-        </div>
-        <!-- /column -->
-        <div
-          class="md:w-10/12 md:!ml-[8.33333333%] lg:!ml-0 lg:w-5/12 xl:!ml-0 xl:w-5/12 w-full flex-[0_0_auto] !px-[15px] xl:!px-[35px] lg:!px-[20px] !mt-[50px] xl:!mt-0 max-w-full text-center lg:text-left xl:text-left"
-        >
-          <h1
-            class="xl:!text-[2.7rem] !text-[calc(1.395rem_+_1.74vw)] !leading-[1.15] !mb-5 md:mx-[-1.25rem] lg:mx-0 !mt-7"
-          >
-            Who We Are
-          </h1>
-          <p>RunnableWeb is a digital software agency specializing in</p>
-          <h1>
-            <br class="xl:!hidden lg:!hidden md:!hidden" /><span
-              class="rotator-fade !text-[#5eb9f0]"
-            >
-              {{ currentText }}
-            </span>
+    <div class="container pt-10 pb-[4.5rem] text-center">
+      <div class="flex flex-wrap items-center">
+        <div class="xl:w-5/12 w-full text-center lg:text-left px-4">
+          <h1 class="text-[2rem] leading-[1.15] mb-5 mt-7">Who We Are</h1>
+          <p class="text-[1.1rem] leading-[1.55] font-medium mb-4">
+            RunnableWeb is a digital software agency specializing in
+          </p>
+
+          <h1 class="h-10 relative overflow-hidden">
+            <transition name="slide" mode="out-in">
+              <span
+                :key="currentTextKey"
+                class="block absolute text-[#5eb9f0] text-3xl"
+              >
+                {{ currentText }}
+              </span>
+            </transition>
           </h1>
 
-          <p class="lead !text-[1.1rem] !leading-[1.55] font-medium !mb-7">
-            <!-- We are an award winning design agency that strongly believes in the
-            power of creative ideas. -->
-
+          <p class="text-[1.1rem] leading-[1.55] font-medium mb-7">
             We combine proven technologies with modern development workflows to
             deliver scalable, high-quality solutions—fast. <br />
             We don't just build apps. We<strong> partner with you </strong> to
             design and deliver impactful software products that evolve with your
             business.
           </p>
-          <span
-            ><a
-              class="btn btn-lg btn-sky !text-white !bg-[#5eb9f0] border-[#5eb9f0] hover:text-white hover:!bg-[#5eb9f0] hover:!border-[#5eb9f0] focus:shadow-[rgba(88,167,216,1)] active:text-white active:!bg-[#5eb9f0] active:border-[#5eb9f0] disabled:text-white disabled:!bg-[#5eb9f0] disabled:border-[#5eb9f0] !rounded-[50rem] !mr-2"
-              >Get Started</a
-            ></span
-          >
-        </div>
-        <!-- /column -->
-      </div>
-      <!-- /.row -->
-      <!-- <HomeClients></HomeClients> -->
-      <!-- /.row -->
-    </div>
-    <!-- /.container -->
 
-    <!-- /.overflow-hidden -->
+          <a
+            class="btn btn-lg btn-sky text-white bg-[#5eb9f0] rounded-full"
+            href="#"
+          >
+            Get Started
+          </a>
+        </div>
+
+        <div class="xl:w-7/12 w-full px-4">
+          <figure>
+            <img
+              src="~/assets/img/illustrations/i21.png"
+              srcset="~/assets/img/illustrations/i21@2x.png 2x"
+              alt="image"
+            />
+          </figure>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -72,25 +56,42 @@ export default {
       texts: ["Cross-Platform", "Cloud-Connected", "AI-Accelerated"],
       currentIndex: 0,
       currentText: "Cross-Platform",
+      currentTextKey: 0,
     };
   },
   mounted() {
-    this.startTextRotation(2000); // Change text every 2 seconds
+    this.startTextRotation(3000);
   },
   methods: {
     startTextRotation(interval) {
       setInterval(() => {
         this.currentIndex = (this.currentIndex + 1) % this.texts.length;
         this.currentText = this.texts[this.currentIndex];
+        this.currentTextKey++;
       }, interval);
     },
   },
 };
 </script>
-
 <style scoped>
-div {
-  font-size: 24px;
-  text-align: center;
+.slide-enter-active {
+  transition: all 1.2s ease;
+}
+.slide-leave-active {
+  transition: opacity 0.1s ease;
+}
+.slide-enter-from {
+  opacity: 0;
+  transform: translateY(-100%);
+}
+.slide-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+.slide-leave-from {
+  opacity: 0;
+}
+.slide-leave-to {
+  opacity: 0;
 }
 </style>
